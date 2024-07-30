772bb3b97fefb72439a1bae6e11f8db5b05e6c3a
Marco <marcodutto1994@gmail.com>
2024-07-30 10:57:28 -0300

    se retocan algunas decoraciones como hover y color de texto

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 const Item = ( { producto } ) => {
     return (
         <Link to={'/detalle/'+producto.id}>
+            <div className="cardS" >
             <img src={producto.cover} alt="" />
             <p>{producto.title}</p>
             <p>{producto.price}</p>
+            </div>
         </Link>
     )
 }
