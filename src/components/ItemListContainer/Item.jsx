6394b5732b9ed2c07b2ae6ca9d4730a8ec2ce3bc
Marco <marcodutto1994@gmail.com>
2024-08-22 12:08:34 -0300

    se agrega detalle de hover a las cards usando eventos sinteticos

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 const Item = ( { producto } ) => {
+    const [expandir, setExpandir] = useState(false)
+
+    const handleMouseOver = () => {
+        setExpandir(true)
+    }
+
+    const handleMouseAway = () => {
+        setExpandir(false)
+    }
+
+    const cardStyle = {
+        transform: expandir ? 'scale(1.1)' : 'scale(1)',
+        transition: 'transform 0.3s ease-in-out',
+    }
+
     return (
-        <Link to={'/detalle/'+producto.id}>
+        <Link to={'/detalle/'+producto.id}
+        onMouseOver={handleMouseOver}
+        onMouseLeave={handleMouseAway}
+        style={cardStyle}
+        className={expandir ? "cardExpandida" : "card"}
+        >
+
             <div className="cardS" >
             <img src={producto.cover} alt="" />
             <p>{producto.title}</p>
